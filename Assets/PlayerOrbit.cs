8f250bdadf7777ac01c5a7b6aabf5534380ed7f8
Paul Sinnett <paul.sinnett@gmail.com>
2018-01-30 17:56:52 +0000

    Initial commit

---
+++ Assets/PlayerOrbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOrbit : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float turnSpeed = 1.0f;
+
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Rotate(Vector3.right, speed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
